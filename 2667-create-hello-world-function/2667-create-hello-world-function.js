1ff1e365b5a57a6eeadfee6b6c59e6c40051b5bd
venera <veneraaibekova35@gmail.com>
2024-07-03 16:25:39 +0600

    Time: 63 ms (5.26%), Space: 48.6 MB (52.91%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,9 @@
+ var createHelloWorld = function() {
+    
+    return function(...args) { return "Hello World";        
+    }
+};
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
