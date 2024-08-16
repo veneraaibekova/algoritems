5076c5d5308d03d799026dc709a06b42b01b5797
venera <veneraaibekova35@gmail.com>
2024-08-16 22:31:25 +0600

    Time: 55 ms (31.6%), Space: 49 MB (11.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -7,13 +7,13 @@
             return n;
         }
         
-        let lift = new Array(n + 1);
-        lift[1] = 1;
-        lift[2] = 2;
+        let arr = new Array(n + 1);
+        arr[1] = 1;
+        arr[2] = 2;
         
         for (let i = 3; i <= n; i++) {
-            lift[i] = lift[i-1] + lift[i-2];
+            arr[i] = arr[i-1] + arr[i-2];
         }
         
-        return lift[n];
+        return arr[n];
     };
